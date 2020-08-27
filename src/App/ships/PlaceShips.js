3479cfd21a551f8f8728cc5a44f3f1cd7e9cef65
StiopAlexandra <StiopAlexandra@users.noreply.github.com>
2020-08-27 15:49:04 +0300

    second commit

---
+++ src/App/ships/PlaceShips.js
@@ -0,0 +1,98 @@
+const isOccupied = (grid, row, col, ships, currentShip, rotated) => {
+  let isTaken = false;
+  if (rotated) {
+    if (row + ships[currentShip].size <= 11) {
+      for (let i = 0; i < ships[currentShip].size; i++) {
+        if (grid[row + i][col].status === "occupied") {
+          isTaken = true;
+        }
+      }
+    }
+  } else {
+    if (col + ships[currentShip].size <= 11) {
+      for (let i = 0; i < ships[currentShip].size; i++) {
+        if (grid[row][col + i].status === "occupied") {
+          isTaken = true;
+        }
+      }
+    }
+  }
+  return isTaken;
+};
+
+export const placeShip = ({ grid, row, col, ships, currentShip, rotated }) => {
+  if (isOccupied(grid, row, col, ships, currentShip, rotated)) {
+    return null;
+  } else {
+    if (rotated) {
+      if (row + ships[currentShip].size <= 11) {
+        for (let i = 0; i < ships[currentShip].size; i++) {
+          grid[row + i][col].status = "occupied";
+          grid[row + i][col].type = "";
+          grid[row + i][col].hover = false;
+          ships[currentShip].positions.push({ row: row + i, col, hit: false });
+        }
+        return {
+          grid,
+          ships,
+        };
+      }
+    } else {
+      if (col + ships[currentShip].size <= 11) {
+        for (let i = 0; i < ships[currentShip].size; i++) {
+          grid[row][col + i].status = "occupied";
+          grid[row][col + i].type = "";
+          grid[row][col + i].hover = false;
+          ships[currentShip].positions.push({ row, col: col + i, hit: false });
+        }
+        return {
+          grid,
+          ships,
+        };
+      }
+    }
+  }
+  return null;
+};
+
+export const hoverUpdate = ({
+  grid,
+  row,
+  col,
+  ships,
+  currentShip,
+  type,
+  rotated,
+}) => {
+  const bool = type === "enter" ? true : false;
+  if (rotated) {
+    if (row + ships[currentShip].size <= 11) {
+      for (let i = 0; i < ships[currentShip].size; i++) {
+        grid[row + i][col].hover = bool;
+      }
+    }
+  } else {
+    if (col + ships[currentShip].size <= 11) {
+      for (let i = 0; i < ships[currentShip].size; i++) {
+        grid[row][col + i].hover = bool;
+      }
+    }
+  }
+  return grid;
+};
+
+export const classUpdate = (square) => {
+  let classes = "grid-square ";
+  if (square.status === "occupied" && square.hover) {
+    classes += "active-occupied";
+  } else if (square.hover) {
+    classes += "active";
+  } else if (square.status === "occupied") {
+    classes += "occupied";
+  } else if (square.status === "hit") {
+    classes += "hit";
+  } else if (square.status === "sunk") {
+    classes += "sunk";
+  }
+  return classes;
+};
